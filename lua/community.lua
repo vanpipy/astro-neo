fbb19a6d656bfae9435b619a58a8ef5872861b69
vanpiy <vanpipy@gmail.com>
2025-01-20 19:49:18 +0800

    chore: enable the vue features

--- lua/community.lua
+++ lua/community.lua
@@ -10,4 +10,5 @@ return {
   -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.vue" },
 }
